cde69108abe9f8a1095ce338dede963b201545a9
AxelRonny <127845742+AxelRonny@users.noreply.github.com>
2023-06-17 01:40:27 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,6 @@
 resource "azurerm_resource_group" "webserver" {
-   name = "ar-jenkins-server"
+   name = "brief14-axel-${var.environment}"
    location = var.location
-}
-# Connect the security group to the network interface
-resource "azurerm_network_interface_security_group_association" "webserver" {
-  network_interface_id      = azurerm_network_interface.webserver.id
-  network_security_group_id = azurerm_network_security_group.allowedports.id
 }
 
 resource "azurerm_network_security_group" "allowedports" {
@@ -65,7 +60,7 @@ resource "azurerm_public_ip" "webserver_public_ip" {
 }
 
 resource "azurerm_network_interface" "webserver" {
-   name = "jenkins-interface"
+   name = "brief14-interface"
    location = azurerm_resource_group.webserver.location
    resource_group_name = azurerm_resource_group.webserver.name
 
@@ -79,9 +74,9 @@ resource "azurerm_network_interface" "webserver" {
    depends_on = [azurerm_resource_group.webserver]
 }
 
-resource "azurerm_linux_virtual_machine" "jenkins" {
+resource "azurerm_linux_virtual_machine" "Brief14" {
    size = var.instance_size
-   name = "jenkins-webserver"
+   name = "brief14-axelwebserver"
    resource_group_name = azurerm_resource_group.webserver.name
    location = azurerm_resource_group.webserver.location
    custom_data = base64encode(file("init-script.sh"))
@@ -92,17 +87,17 @@ resource "azurerm_linux_virtual_machine" "jenkins" {
    source_image_reference {
        publisher = "Canonical"
        offer = "UbuntuServer"
-       sku = "20.04-LTS"
+       sku = "18.04-LTS"
        version = "latest"
    }
 
-   computer_name = "jenkins"
-   admin_username = "adminuserar"
-   admin_password = "ronnyand2002@yahoo.fr"
+   computer_name = "brief14"
+   admin_username = "axel"
+   admin_password = "Ronnyand2002@yahoo.fr"
    disable_password_authentication = false
 
    os_disk {
-       name = "jenkinsdisk01"
+       name = "brief14disk01"
        caching = "ReadWrite"
        #create_option = "FromImage"
        storage_account_type = "Standard_LRS"
